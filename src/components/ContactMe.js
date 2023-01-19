01bb7b3b5ad3be91e5fc256df8c8ef2e33e2e408
huifang <hui-f.gu@hotmail.com>
2023-01-19 15:34:01 -0800

    css changes on nav for deploy

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -31,8 +31,8 @@ const ContactMe = () => {
       <Row className="mb-3">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3276.452078262283!2d138.4948624148116!3d-34.79456657512722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ab0b9f802cef741%3A0x7574ce7728cb85a4!2sLightStory%20Photography!5e0!3m2!1sen!2sau!4v1674043201351!5m2!1sen!2sau" width="100%" height="500px" style={{ border: 0 }} allowfullscreen="" loading="eager" referrerPolicy="no-referrer-when-downgrade" />
       </Row>
-      <Row className="d-flex">
-        <Col xs={12} lg={6} md={6}>
+      <Row className="d-flex mb-2">
+        <Col xs={12} lg={6} md={6} className="mb-2">
           <motion.div
             xs={12}
             lg={6}
@@ -59,7 +59,7 @@ const ContactMe = () => {
             </Card>
           </motion.div>
         </Col>
-        <Col xs={12} lg={6} md={6}>
+        <Col xs={12} lg={6} md={6} className="mb-2">
           <motion.div
             ref={ref}
             variants={boxVariant}
