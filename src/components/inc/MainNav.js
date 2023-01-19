c4c6fcf6c771294760d41fde255b273c316fe2e3
huifang <hui-f.gu@hotmail.com>
2023-01-19 15:22:17 -0800

    css changes on nav for deploy

--- src/components/inc/MainNav.js
+++ src/components/inc/MainNav.js
@@ -6,7 +6,7 @@ import brand from '../image/logo.png';
 const MainNav = () => {
   return (
     <Navbar variant="dark" expand="lg">
-      <Container fluid style={{ backgroudColor: '#14213d', width: '85%' }}>
+      <Container className="navContainer" fluid>
         <Navbar.Brand as={Link} to="/">
           <Image className="brand" src={brand} />
         </Navbar.Brand>
