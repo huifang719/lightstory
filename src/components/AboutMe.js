d516cd75c3805ddf5dcc2bf7dcc51b73536ba67b
huifang <hui-f.gu@hotmail.com>
2023-01-19 20:52:40 +1030

    change logo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Col, Container, Row, Image } from 'react-bootstrap';
+import profile from './image/profile.png';
 
 const AboutMe = () => {
   return (
     <Container>
       <Row>
-        <Col className="sm-12 lg-6 md-6">
-          <Image src="https://images.unsplash.com/photo-1673973059807-857c544d8452?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+        <Col xs={12} lg={6} md={6}>
+          <Image style={{ width: '80%' }} src={profile} />
         </Col>
-        <Col className="sm-12 lg-6 md-6">
+        <Col xs={12} lg={6} md={6}>
           <h3>
             About Me
           </h3>
