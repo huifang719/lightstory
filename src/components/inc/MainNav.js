ed5bb91f9a7115c28f5f44a422aa20cc7c92949d
huifang <hui-f.gu@hotmail.com>
2023-01-20 12:18:30 +1030

    css changes on navbar for deploying

--- src/components/inc/MainNav.js
+++ src/components/inc/MainNav.js
@@ -6,7 +6,7 @@ import brand from '../image/logo.png';
 const MainNav = () => {
   return (
     <Navbar variant="dark" expand="lg">
-      <Container className="navContainer" fluid>
+      <Container>
         <Navbar.Brand as={Link} to="/">
           <Image className="brand" src={brand} />
         </Navbar.Brand>
