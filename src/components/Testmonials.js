83404010c84220a32ebdbcbe981bcc566fdac386
huifang <hui-f.gu@hotmail.com>
2023-01-24 13:38:46 -0800

    update welcome session

--- src/components/Testmonials.js
+++ src/components/Testmonials.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 const Testmonials = () => {
+  const [reviews, setReviews] = useSate([]);
+
+  const getReviews = async () = {
+    const clientID = '156810727351-i87vikc9cqt2ti5eik59b55vhmt02eqj.apps.googleusercontent.com';
+    const response = await fetch(
+      'https://mybusiness.googleapis.com/v4/accounts/10800535903591651999/locations/ChIJs8i596K4sGoR9U4iYSV6d54/reviews',
+      {
+        method: 'GET',
+        headers: {
+          'clientID': clientID,
+          'secret': process.env.REACT-APP-GOOGLE-SECRET
+        }
+      }
+    )
+
+  }
   return (
     <Container fluid className="pageContainer">
       <div data-token="IU4GRsIL3wsrJsusynAmWlP7A2QFuzrUhbLUCnBekKzGL1y3YQ" className="romw-reviews" />
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Card } from 'react-bootstrap';
-import Typewriter from 'typewriter-effect';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
-const Welcome = () => (
-  <Container fluid className="pt-3 pb-3" style={{ backgroundColor: 'white', color: '#14213d', display: 'flex', justifyContent: 'center' }}>
-    <Card className="welcome">
-      <Card.Body>
-        <Card.Title style={{ textAlign: 'center' }}>Welcome</Card.Title>
-        <Card.Subtitle className="mb-2">Let me help you to hold on to your beautiful memories.</Card.Subtitle>
-        <Card.Text>
-          <Typewriter
-            style={{ fontSize: '1rem' }}
-            options={{
-              strings: ['Memory is a way of holding on to the things you love, the things you are,the things you never want to lose.'],
-              autoStart: true,
-              loop: true,
-              deleteAll: 'natural',
-            }}
-          />
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </Container>
-);
+const Welcome = () => {
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, scale: 0, y: 30 },
+  };
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    } else {
+      control.start('hidden');
+    }
+  }, [control, inView]);
+
+  return (
+    <Container fluid className="pt-3 pb-3" style={{ backgroundColor: 'white' }}>
+      <motion.div
+        style={{ backgroundColor: 'white', color: '#14213d', display: 'flex', justifyContent: 'center' }}
+        fluid
+        xs={12}
+        lg={6}
+        md={6}
+        ref={ref}
+        variants={boxVariant}
+        initial="hidden"
+        animate={control}
+      >
+        <Card className="welcome">
+          <Card.Body>
+            <Card.Title style={{ textAlign: 'center' }}>Welcome</Card.Title>
+            <Card.Subtitle className="mb-2">Memory is a way of holding on to the things you love, the things you are,the things you never want to lose.</Card.Subtitle>
+            <Card.Text>
+              Let me help you to hold on to your beautiful memories.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </motion.div>
+    </Container>
+  );
+};
 
 export default Welcome;
