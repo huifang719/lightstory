8167d4001527447c608963c9ed73ed4343889b20
huifang <hui-f.gu@hotmail.com>
2023-01-24 14:34:21 -0800

    update welcome session

--- src/components/Testmonials.js
+++ src/components/Testmonials.js
@@ -1,23 +1,30 @@
+/* eslint-disable no-undef */
+/* eslint-disable quote-props */
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-infix-ops */
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 const Testmonials = () => {
-  const [reviews, setReviews] = useSate([]);
+  const [reviews, setReviews] = useState([]);
 
-  const getReviews = async () = {
+  const getReviews = async () => {
     const clientID = '156810727351-i87vikc9cqt2ti5eik59b55vhmt02eqj.apps.googleusercontent.com';
-    const response = await fetch(
-      'https://mybusiness.googleapis.com/v4/accounts/10800535903591651999/locations/ChIJs8i596K4sGoR9U4iYSV6d54/reviews',
+    const businessID = process.env.REACT-APP-BUSINESS-ID;
+    const placeID = process.env.REACT-APP-PLACE-ID;
+    const secret = process.env.REACT-APP-GOOGLE-SECRET;
+    const res = await fetch(
+      `https://mybusiness.googleapis.com/v4/accounts/${businessID}/locations/${placeID}/reviews`,
       {
         method: 'GET',
         headers: {
           'clientID': clientID,
-          'secret': process.env.REACT-APP-GOOGLE-SECRET
-        }
-      }
-    )
+          'secret': secret,
+        },
+      },
+    );
+  };
 
-  }
   return (
     <Container fluid className="pageContainer">
       <div data-token="IU4GRsIL3wsrJsusynAmWlP7A2QFuzrUhbLUCnBekKzGL1y3YQ" className="romw-reviews" />
