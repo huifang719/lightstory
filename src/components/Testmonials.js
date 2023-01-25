24bc9596a7b625263a76d1862e38d9efbf7d4c00
huifang <hui-f.gu@hotmail.com>
2023-01-25 12:50:34 +1030

    finalise testmonials

--- src/components/Testmonials.js
+++ src/components/Testmonials.js
@@ -4,8 +4,25 @@
 /* eslint-disable space-infix-ops */
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Testmonials = () => {
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, scale: 0, y: 30 },
+  };
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    } else {
+      control.start('hidden');
+    }
+  }, [control, inView]);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://reviewsonmywebsite.com/js/embedLoader.js?id=03d30ab029f3709eb7ee597570569e34';
@@ -18,8 +35,20 @@ const Testmonials = () => {
 
   return (
     <Container fluid className="pt-3 pb-3" style={{ backgroundColor: 'white' }}>
-      <h5 className="mt-3 mb-3" style={{ textAlign: 'center', color: '#14213d' }}>Testmonials</h5>
-      <div data-token="IU4GRsIL3wsrJsusynAmWlP7A2QFuzrUhbLUCnBekKzGL1y3YQ" className="romw-reviews" />
+      <motion.div
+        style={{ backgroundColor: 'white', color: '#14213d', display: 'flex', justifyContent: 'center' }}
+        fluid
+        xs={12}
+        lg={6}
+        md={6}
+        ref={ref}
+        variants={boxVariant}
+        initial="hidden"
+        animate={control}
+      >
+        <h5 className="mt-3 mb-3" style={{ textAlign: 'center', color: '#14213d' }}>Testmonials</h5>
+        <div data-token="IU4GRsIL3wsrJsusynAmWlP7A2QFuzrUhbLUCnBekKzGL1y3YQ" className="romw-reviews" />
+      </motion.div>
     </Container>
   );
 };
