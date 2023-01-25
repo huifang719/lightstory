c6d73fefbfddce5f3ac85278e8c253ce09f805b2
huifang <hui-f.gu@hotmail.com>
2023-01-25 16:01:22 +1030

    add footer

--- src/components/inc/Footer.js
+++ src/components/inc/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Container, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import SocialMedia from '../SocialMedia';
 
 const Footer = () => {
   return (
-    <Container className="mt-3 d-flex justify-content-center align-items-center">
-      <Col xs={12} lg={6} md={6}>
-        <p style={{ textAlgin: 'center' }}><small>Copyright &copy; 2023 LightStory Photography | All rights All Rights Reserved</small></p>
-      </Col>
-      <Col xs={12} lg={6} md={6}>
-        <SocialMedia />
-      </Col>
+    <Container className="mt-3">
+      <Row fluid className="d-flex align-items-center">
+        <Col xs={12} lg={6} md={6}>
+          <p style={{ textAlgin: 'center' }}><small>Copyright &copy; 2023 LightStory Photography | All rights All Rights Reserved</small></p>
+        </Col>
+        <Col xs={12} lg={6} md={6}>
+          <SocialMedia />
+        </Col>
+      </Row>
     </Container>
   );
 };
